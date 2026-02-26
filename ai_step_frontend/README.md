e309f967204e91b7c5257b0171fa8328e9905206
Kirill Golubenko <golubenkokirill@outlook.com>
2026-02-26 19:14:18 +0500

    feat: remove unnecessary files

--- ai_step_frontend/README.md
+++
@@ -1,3 +0,0 @@
-# ai_step_frontend
-
-A new Flutter project.
--- ai_step_frontend/android/app/src/main/kotlin/com/example/ai_step_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ai_step_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
